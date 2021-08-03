d4e557f67e6cacc7e321557aa13341875cd9ba89
swarangimp05 <86718547+swarangimp05@users.noreply.github.com>
2021-08-03 14:17:43 +0530

    homepage ,media

--- webpage/src/App.js
+++ webpage/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import {Home} from './components';
+import {About} from './components';
 function App() {
   return (
     <div>
@@ -9,6 +10,7 @@ function App() {
      <Router>
         <Route exact path="/">
           <Home/>
+          <About/>
         </Route>
       </Router>
 
--- webpage/src/components/Home.jsx
+++ webpage/src/components/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import img1 from "../media/img1.svg";
+import './Home.css';
+
 const Home = () => {
     return (
-        <div>
-            hi all
-        </div>
+        <div class="container">
+           <h1> Hi All..Welcome!!
+           </h1>
+        <img class="logo-img" src={img1}alt="logo"></img>
+      </div>
       )
                     
 }
