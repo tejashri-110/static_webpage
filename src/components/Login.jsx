7ab576fd4fa5741d8142ba7fe6f24ca8303aee89
Tejashri Mitbavkar <mitbavkartejashri@gmail.com>
2021-08-11 11:12:21 +0530

    home changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,27 +3,27 @@ import "./Login.css"
 import "./Login.js"
 
 const Login = () => {
-    return (
-        <form>
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" />
-        </div>
-        <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-          <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-        </div>
-        <button formAction="register.html" onclick="register()">Register</button>
-        <button formAction="success.html" onclick="login()">Login</button>
-      </form>
+  return (
+    <form>
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+        <input type="password" className="form-control" id="exampleInputPassword1" />
+      </div>
+      <div className="mb-3 form-check">
+        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+      </div>
+      <button formAction="register.html" onclick="register()">Register</button>
+      <button formAction="success.html" onclick="login()">Login</button>
+    </form>
 
-        )
-        }
+  )
+}
 
-    // export default Login
+export default Login
 
